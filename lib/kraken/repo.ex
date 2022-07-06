f99a1add6ef7423b70f084f7e4c80395fd146e76
Jon-Eric Cook <joneric.cook@blockfi.com>
2022-07-06 09:35:48 -0700

    save work

--- lib/kraken/repo.ex
+++ lib/kraken/repo.ex
@@ -6,11 +6,11 @@ defmodule Kraken.Repo do
   def init(_, config) do
     config =
       config
-      |> Keyword.put(:username, System.get_env("PGUSER"))
-      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
-      |> Keyword.put(:database, System.get_env("PGDATABASE"))
-      |> Keyword.put(:hostname, System.get_env("PGHOST"))
-      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer())
+      |> Keyword.put(:username, System.get_env("PGUSER", "postgres"))
+      |> Keyword.put(:password, System.get_env("PGPASSWORD", "postgres"))
+      |> Keyword.put(:database, System.get_env("PGDATABASE", "kraken_repo"))
+      |> Keyword.put(:hostname, System.get_env("PGHOST", "localhost"))
+      |> Keyword.put(:port, System.get_env("PGPORT", "5432") |> String.to_integer())
 
     {:ok, config}
   end
